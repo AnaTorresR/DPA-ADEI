7032f50162a8c9f5461ee664ed2a5ab57f5dbf7b
EddOselotl <ing.edbaz@gmail.com>
2021-02-18 12:12:11 -0600

    Update general.py
    Se agregan funciones para guardar y crear pickles

--- src/utils/general.py
+++ src/utils/general.py
@@ -1,15 +1,17 @@
 import yaml
+import pickle
+
 
 def read_yaml_file(yaml_file):
     """ load yaml cofigurations """
 
     config = None
-    try: 
-        with open (yaml_file, 'r') as f:
+    try:
+        with open(yaml_file, 'r') as f:
             config = yaml.safe_load(f)
     except:
         raise FileNotFoundError('Couldnt load the file')
-    
+
     return config
 
 
@@ -19,3 +21,15 @@ def get_s3_credentials(credentials_file):
 
     return s3_creds
 
+
+def load_pickle(path):
+    data_pkl = pickle.load(open(path, "rb"))
+    return data_pkl
+
+
+def save_pickle(df, path):
+    # '/full/path/to/file'
+    with open(path, 'wb') as f:
+        pickle.dump(df, f)
+        f.close()
+    return
