b64fd2f2e280aff38c5ab2e6c9420dbbbe9e4b11
EddOselotl <ing.edbaz@gmail.com>
2021-02-22 15:30:56 -0600

    Update ingesta_almacenamiento.py
    Se modifico codigo para utilizar el script constants.py que contienen el nombre del bucket del equipo y la informacion del data set domain e identificador de Food Inspection para trabajar la api .
    Se modifica:
    get_client(token):
    ingesta_inicial(client):
    ingesta_consecutiva(client, limit):
    
    para trabajar con las constantes dentro de las funciones y no pasarlas como parametros

--- src/pipeline/ingesta_almacenamiento.py
+++ src/pipeline/ingesta_almacenamiento.py
@@ -5,22 +5,28 @@ from datetime import timedelta
 import boto3
 import pickle
 from src.utils.general import get_s3_credentials
+from src.utils import constants
 
 
-def get_client(dataset_domain, token):
+def get_client(token):
     # Example authenticated client (needed for non-public datasets):
+    # extraemos el data set domain de nuestro script src/utils/constants
+    dataset_domain = constants.dataset_domain
     client = Socrata(dataset_domain, token)
 
     return client
 
 
-def ingesta_inicial(client, dataset_id):
+def ingesta_inicial(client):
     # Returned as JSON from API / converted to Python list of
     # dictionaries by sodapy.
 
     # se pasa el data_set identifier para obtener los datos
     # regresa todos los registros que existan hasta el momento
     # para este proyecto el data set cuenta con mas menos 216k de registros
+
+    # extraemos el dataset identifier de nuestro script src/utils/constants
+    dataset_id = constants.dataset_id
     print(dataset_id)
     results = client.get_all(dataset_id)
     results_df = pd.DataFrame.from_records(results)
@@ -29,9 +35,10 @@ def ingesta_inicial(client, dataset_id):
     obj_to_upload = pickle.dumps(results_df)
 
     # configuración  para la carga en s3 bucket
-    bucket_name = "data-product-architecture-equipo-6"
+    # bucket_name = "data-product-architecture-equipo-6"
+    # se extrae el nombre del bucket de nuestro script src/utils/constants
+    bucket_name = constants.bucket_name
     bucket_path = "ingestion/initial/historic-inspections-{}.pkl".format(str(date.today()))
-    # file_name = bucket_path + path_pkl.split(sep='/')[-1]
 
     # se llama a función guardar ingesta
     guardar_ingesta(bucket_name, bucket_path, obj_to_upload)
@@ -39,12 +46,15 @@ def ingesta_inicial(client, dataset_id):
     return
 
 
-def ingesta_consecutiva(client, dataset_id, limit):
+def ingesta_consecutiva(client, limit):
     # se utiliza timedelta para restar 7 dias al dia de hoy
     # para traer semanalmente los delta registros
     today = date.today()
     delta_date = today - timedelta(days=7)
     print(delta_date)
+
+    # extraemos el dataset identifier de nuestro script src/utils/constants
+    dataset_id = constants.dataset_id
 
     # se hace un where con la seleccion de 7 dias antes para traer deltas
     results = client.get(dataset_id, where="inspection_date >= '{}'".format(delta_date), limit=limit)
@@ -54,9 +64,10 @@ def ingesta_consecutiva(client, dataset_id, limit):
     obj_to_upload = pickle.dumps(results_df)
 
     # configuración  para la carga en s3 bucket
-    bucket_name = "data-product-architecture-equipo-6"
+    # bucket_name = "data-product-architecture-equipo-6"
+    # se extrae el nombre del bucket de nuestro script src/utils/constants
+    bucket_name = constants.bucket_name
     bucket_path = "ingestion/consecutive/consecutive-inspections-{}.pkl".format(str(today))
-    # file_name = bucket_path + path_pkl.split(sep='/')[-1]
 
     # se llama a función gradar ingesta
     guardar_ingesta(bucket_name, bucket_path, obj_to_upload)
@@ -79,6 +90,7 @@ def guardar_ingesta(bucket_name, bucket_path, obj_to_upload):
 
     # accedemos a client desde el resource y se hace upload de documentos
     # s3_resource.meta.client.upload_file(file_to_upload, bucket_name, bucket_path)
+
     # esto es para guardar objetos
     s3_resource.put_object(Bucket=bucket_name, Key=bucket_path, Body=obj_to_upload)
 
