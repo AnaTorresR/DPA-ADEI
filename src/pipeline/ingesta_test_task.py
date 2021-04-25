9ea56095b839d19ce590a5b53f1d66ee636e1417
diramtz <nancy.dira@gmail.com>
2021-04-25 14:39:29 -0500

    prueba unit test ingesta

--- src/pipeline/ingesta_test_task.py
+++ src/pipeline/ingesta_test_task.py
@@ -1,4 +1,55 @@
 import luigi
-import marbles.core
+import luigi.contrib.s3
+import pickle
+from src.utils import constants
+from src.pipeline.ingesta_task import IngestaTask
+from src.utils.general import get_s3_credentials, get_db_credentials
+from src.utils import unittests2
+from luigi.contrib.postgres import CopyToTable
 
-class IngestaTest(luigi.Task):
+# PYTHONPATH='.' luigi --module src.pipeline.ingesta_test_task TestIngestaTask --ingesta consecutiva --year 2021 --month 04 --day 15
+
+class TestIngestaTask(CopyToTable):
+    ingesta = luigi.Parameter()
+    year = luigi.Parameter()
+    month = luigi.Parameter()
+    day = luigi.Parameter()
+
+    def requires(self):
+        return {
+        'IngestaTask': IngestaTask(self.ingesta, self.year, self.month, self.day)}
+
+    credentials = get_db_credentials('conf/local/credentials.yaml')
+
+    user=credentials['user']
+    password=credentials['pass']
+    host=credentials['host']
+    port=credentials['port']
+    database=credentials['db']
+
+    table = 'metadata'
+
+    columns = [("Task", "VARCHAR"),
+               ("INGESTION", "VARCHAR"),
+               ("FECHA", "TIMESTAMP WITHOUT TIME ZONE"),
+               ("AUTOR", "VARCHAR")]
+
+    def rows(self):
+
+        if self.ingesta == 'historica':
+            temp_path = 'temp/{}/{}-{}-{}-{}.pkl'. \
+                format(constants.bucket_name, constants.initial_path, self.year, self.month, self.day)
+        elif self.ingesta == 'consecutiva':
+            temp_path = 'temp/{}/{}-{}-{}-{}.pkl'. \
+                format(constants.bucket_name, constants.concecutive_path, self.year, self.month, self.day)
+        else:
+            print('No such type of ingestion')
+
+        df = load_pickle_file(temp_path)
+
+        TestIngesta(df)
+
+        date = str(self.year + '-' + self.month + '-' + self.day)
+        r = [("unit test ingesta","Ingesta {}".format(self.ingesta), date , 'Equipo 6')]
+        for element in r:
+            yield element
--- src/utils/unittests.py
+++ src/utils/unittests.py
@@ -11,17 +11,22 @@ class TestIngesta(marbles.core.TestCase,  mixins.CategoricalMixins, mixins.FileM
     path="/home/diramtz/Documents/DPA/DPA-food_inspections/temp/data-product-architecture-equipo-6/ingestion/consecutive/consecutive-inspections-2021-03-17.pkl"
     df = load_pickle_file(path)
 
-    RISKS = ['Risk 1 (High)', 'Risk 2 (Medium)', 'Risk 3 (Low)']
+    RISKS = ['Risk 1 (High)', 'Risk 2 (Medium)', 'Risk 3 (Low)', 'All']
 
     msg_file = "El archivo pkl de ingesta no existe."
     msg_risks = "Los niveles de Risks de la nueva ingesta no coinciden con los datos anteriores."
     msg_date = "La ingesta contiene datos del futuro o del pasado."
+    msg_col = "El número de columnas no coincide con los datos anteriores."
+    msg_row = "La ingesta está vacía, no contiene ninguna observación."
 
     def test_file_exits(self):
         self.assertFileExists(self.path, msg = self.msg_file)
 
     def test_categories_risks(self):
-        self.assertCategoricalLevelsEqual(set(self.df['risk']), self.RISKS, msg= self.msg_risks)
+        A = set(self.df['risk'])
+        B = set(self.RISKS)
+        A.issubset(B)
+        self.assertTrue(A.issubset(B), msg= self.msg_risks)
 
     def test_inspection_date_future(self):
         today = date.today()
@@ -32,12 +37,29 @@ class TestIngesta(marbles.core.TestCase,  mixins.CategoricalMixins, mixins.FileM
         first_insp = datetime.strptime( '04-01-2021', "%d-%m-%Y")
         dates = pd.to_datetime(self.df['inspection_date'])
         self.assertDateTimesAfter(dates, first_insp, msg = self.msg_date)
+
+    def test_num_columns(self):
+        self.assertTrue(len(self.df.columns) == 17, msg = self.msg_col)
+
+    def test_not_empty(self):
+        self.assertTrue(len(self.df.index) > 0, msg = self.msg_row)
 
 class TestAlmacenamiento(marbles.core.TestCase, marbles.mixins.DateTimeMixins):
     path="/home/diramtz/Documents/DPA/DPA-food_inspections/temp/data-product-architecture-equipo-6/ingestion/consecutive/consecutive-inspections-2021-03-17.pkl"
     df = load_pickle_file(path)
 
+    RISKS = ['Risk 1 (High)', 'Risk 2 (Medium)', 'Risk 3 (Low)', 'All']
+
+    msg_risks = "Los niveles de Risks de la nueva ingesta no coinciden con los datos anteriores."
     msg_date = "El almacenamiento contiene datos del futuro o del pasado. Revisar la ingesta."
+    msg_col = "El número de columnas no coincide con los datos anteriores."
+    msg_row = "El almacenamiento está vacío, no contiene ninguna observación. Revisar ingesta."
+
+    def test_categories_risks(self):
+        A = set(self.df['risk'])
+        B = set(self.RISKS)
+        A.issubset(B)
+        self.assertTrue(A.issubset(B), msg= self.msg_risks)
 
     def test_inspection_date_future(self):
         today = date.today()
@@ -48,6 +70,12 @@ class TestAlmacenamiento(marbles.core.TestCase, marbles.mixins.DateTimeMixins):
         first_insp = datetime.strptime( '04-01-2021', "%d-%m-%Y")
         dates = pd.to_datetime(self.df['inspection_date'])
         self.assertDateTimesAfter(dates, first_insp, msg = self.msg_date)
+
+    def test_num_columns(self):
+        self.assertTrue(len(self.df.columns) == 17, msg = self.msg_col)
+
+    def test_not_empty(self):
+        self.assertTrue(len(self.df.index) > 0, msg = self.msg_row)
 
 class TestCleaning(marbles.core.TestCase):
     #cambiar el path cuando hagas luigi
@@ -57,8 +85,7 @@ class TestCleaning(marbles.core.TestCase):
     msg_low = """Existen datos en el data frame con texto que utiliza mayúsuclas.
     Es necasario que todo esté en minúsculas"""
     msg_nas = "El data frame tiene valores faltantes. Revisar que la imputación se hizo correctamente."
-
-    print(df.zip.dtype)
+    msg_col = "El número de columnas no coincide con los datos anteriores."
 
     def test_lower(self):
         risks = self.df.risk
@@ -73,6 +100,8 @@ class TestCleaning(marbles.core.TestCase):
     def test_not_nas(self):
         facilities = self.df.facility_type
         cities = self.df.city
-        print(type(facilities))
         self.assertFalse(facilities.isna().values.any(), msg = self.msg_nas, note = "Columna: facility_type")
         self.assertFalse(cities.isna().values.any(), msg = self.msg_nas, note = "Columna: city")
+
+    def test_num_columns(self):
+        self.assertTrue(len(self.df.columns) == 17, msg = self.msg_col)
