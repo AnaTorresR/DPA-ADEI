b0d9361c1285d92f3e33dbe06bc114df2efc3e4c
Ana <66653295+AnaTorresR@users.noreply.github.com>
2021-04-26 17:20:09 -0500

    Seleccion modelo metadata

---
+++ src/pipeline/seleccion_modelo_metadata_task.py
@@ -0,0 +1,40 @@
+import luigi
+import luigi.contrib.s3
+from luigi.contrib.postgres import CopyToTable
+from src.pipeline.seleccion_modelo_test_task import SeleccionModeloTestTask
+from src.utils.general import get_db_credentials
+from src.utils import constants
+import pickle
+
+# PYTHONPATH='.' luigi --module src.pipeline.seleccion_modelo_metadata_task SeleccionModeloMetadataTask --ingesta consecutiva --year 2021 --month 04 --day 23 --local-scheduler
+
+class SeleccionModeloMetadataTask(CopyToTable):
+    ingesta = luigi.Parameter()
+    year = luigi.Parameter()
+    month = luigi.Parameter()
+    day = luigi.Parameter()
+
+    def requires(self):
+        return SeleccionModeloTestTask(self.ingesta, self.year, self.month, self.day)
+
+    credentials = get_db_credentials('conf/local/credentials.yaml')
+
+    user=credentials['user']
+    password=credentials['pass']
+    host=credentials['host']
+    port=credentials['port']
+    database=credentials['db']
+
+    table = 'metadata'
+
+
+    columns = [("Task", "VARCHAR"),
+               ("INGESTION", "VARCHAR"),
+               ("FECHA", "TIMESTAMP WITH TIME ZONE"),
+               ("AUTOR", "VARCHAR")]
+
+    def rows(self):
+        date = str(self.year + '-' + self.month + '-' + self.day)
+        r = [("Seleccion modelo","Ingesta {}".format(self.ingesta), date , 'Equipo 6')]
+        for element in r:
+            yield element
