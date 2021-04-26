1e6d2293c2f65169955a69ae9f9eac0eb7f980f3
Ana <66653295+AnaTorresR@users.noreply.github.com>
2021-04-26 17:13:07 -0500

    Feature engineering metadata

--- src/pipeline/feature_engineering_metadata_task.py
+++ src/pipeline/feature_engineering_metadata_task.py
@@ -1,12 +1,13 @@
 import luigi
+import luigi
 import luigi.contrib.s3
 from luigi.contrib.postgres import CopyToTable
-from src.pipeline.feature_engineering_task import FETask
+from src.pipeline.feature_engineering_test_task import FETestTask
 from src.utils.general import get_db_credentials
 from src.utils import constants
 import pickle
 
-# PYTHONPATH='.' luigi --module src.pipeline.feature_engineering_metadata_task FEMetadataTask --ingesta consecutiva --year 2021 --month 04 --day 30 --local-scheduler
+# PYTHONPATH='.' luigi --module src.pipeline.feature_engineering_metadata_task FEMetadataTask --ingesta consecutiva --year 2021 --month 03 --day 15 --local-scheduler
 
 class FEMetadataTask(CopyToTable):
     ingesta = luigi.Parameter()
@@ -15,7 +16,7 @@ class FEMetadataTask(CopyToTable):
     day = luigi.Parameter()
 
     def requires(self):
-        return FETask(self.ingesta, self.year, self.month, self.day)
+        return FETestTask(self.ingesta, self.year, self.month, self.day)
 
     credentials = get_db_credentials('conf/local/credentials.yaml')
 
@@ -29,7 +30,7 @@ class FEMetadataTask(CopyToTable):
 
     columns = [("Task", "VARCHAR"),
                ("INGESTION", "VARCHAR"),
-               ("FECHA", "TIMESTAMP WITHOUT TIME ZONE"),
+               ("FECHA", "TIMESTAMP WITH TIME ZONE"),
                ("AUTOR", "VARCHAR")]
 
     def rows(self):
