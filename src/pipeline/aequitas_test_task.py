2c0195c0c886a3e4736016ba4da31e31e8526ddf
diramtz <nancy.dira@gmail.com>
2021-05-04 10:31:56 -0500

    task unittest aequitas

--- src/pipeline/aequitas_test_task.py
+++ src/pipeline/aequitas_test_task.py
@@ -9,3 +9,57 @@ from src.utils.general import *
 from luigi.contrib.postgres import CopyToTable
 
 ## PYTHONPATH='.' luigi --module src.pipeline.aequitas_test_task TestAequitasTask --ingesta consecutiva --year 2021 --month 05 --day 03
+
+class TestAequitasTask(CopyToTable):
+
+    ingesta = luigi.Parameter()
+    year = luigi.Parameter()
+    month = luigi.Parameter()
+    day = luigi.Parameter()
+    model_type = luigi.Parameter(default= 'assistive')
+
+    def requires(self):
+        return AequitasTask(self.ingesta, self.year, self.month, self.day, self.model_type)
+
+    credentials = get_db_credentials('conf/local/credentials.yaml')
+
+    user=credentials['user']
+    password=credentials['pass']
+    host=credentials['host']
+    port=credentials['port']
+    database=credentials['db']
+
+    table = 'tests'
+
+    columns = [("TEST",  "VARCHAR"),
+                ("FECHA", "TIMESTAMP WITH TIME ZONE"),
+                ("AUTOR", "VARCHAR")]
+
+    def rows(self):
+
+        if self.ingesta == 'historica':
+            key = 's3://{}/{}-{}-{}-{}-aequitas.pkl'. \
+                format(constants.bucket_name, constants.initial_path, self.year, self.month, self.day)
+
+        elif self.ingesta == 'consecutiva':
+            key = 's3://{}/{}-{}-{}-{}-aequitas.pkl'. \
+                format(constants.bucket_name, constants.concecutive_path, self.year, self.month, self.day)
+
+        else:
+            print('No such type of ingestion')
+
+        credentials = 'conf/local/credentials.yaml'
+
+        df = load_s3_object(credentials, key)
+
+        tests = unittests.TestAequitas(df, self.year, self.month, self.day)
+
+        tests.test_attributes()
+        tests.test_num_columns()
+        tests.test_not_empty()
+        tests.test_params()
+
+        date_ = str(self.year + '-' + self.month + '-' + self.day)
+        r = [("unit test aequitas", date_ , 'Equipo 6')]
+        for element in r:
+            yield element
